7f88aaed2b3c569e28bfef21a571eea72f33324a
prabum07 <42492071+prabum07@users.noreply.github.com>
2019-09-11 14:07:30 +0530

    checkout To GroundFIx

--- Assets/Scripts/cc.cs
+++ Assets/Scripts/cc.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 
 public class cc : MonoBehaviour
 {
+
+    //to Check
+
     [SerializeField] private Animator animator;
     [SerializeField] public float m_JumpForce ;                          // Amount of force added when the player jumps.
     [SerializeField] public Text m_JumpForceTxt;
